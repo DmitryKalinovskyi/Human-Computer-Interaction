e6907177b297227f8f1e7a5a9ffccb9966f76283
Dmytro <ipz223_kdm@student.ztu.edu.ua>
2024-10-15 00:08:25 +0300

    Initial commit

---
+++ Lab01/Word/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Word
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Lab01/Word/Views/ReferenceWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Wpf.Ui.Controls;
+
+namespace Word.Views
+{
+    /// <summary>
+    /// Interaction logic for ReferenceWindow.xaml
+    /// </summary>
+    public partial class ReferenceWindow : FluentWindow
+    {
+        public ReferenceWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
